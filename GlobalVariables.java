ae9d49423aa571917d4f60f6b4af0cafb6a8cb78
Muhammad Ahmad <151904586+Muhammad-Ahmad42@users.noreply.github.com>
2024-06-25 23:09:47 +0500

    Add files via upload

---
+++ GlobalVariables.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dispatchdesktopapp;
+
+/**
+ *
+ * @author waqasali
+ */
+public class GlobalVariables {
+    public static final String itemFile="Items.txt"; 
+    public static final String TRUCKFILE = "trucks.txt";
+    public static final String DispatchsFile="Dispatch.txt"; 
+    
+}
